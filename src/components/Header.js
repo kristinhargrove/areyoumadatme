72992b5257d7319ccd0875aee0da5512d766677f
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2021-01-28 18:46:43 -0500

    creates header and main component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css'
+
+const Header = (props) => (
+    <nav>
+        <a className="nav-item" href="#">faqs</a>
+        <a className="nav-item" href="#">about</a>
+        <a className="nav-item" href="#">home</a>
+    </nav>
+)
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Main.css'
+const Main = (props) => (
+    <>
+        <body className="main">
+            <h1 className="title">ARE YOU MAD AT ME?</h1>
+            <button className="start">Start</button>
+        </body>
+    </>
+);
+
+export default Main;
