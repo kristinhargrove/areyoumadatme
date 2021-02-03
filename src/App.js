439a4d20188ab83b658a1ed04b85d2a0c106d01b
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2021-02-02 20:52:07 -0500

    updates main page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 
 import Header from './components/Header';
 import Main from './components/Main';
-import Question from './components/Question'
+import Question from './components/Question';
+import ResultsPage from './components/ResultsPage';
+
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 function App() {
   return (
     <>
-      {/* <Header />
-      <Main /> */}
-      <Question />
+      <Provider store={store}>
+        <Header />
+        <Main />
+        <Question />
+        <ResultsPage />
+      </Provider>
     </>
   );
 }
