54f6f03954273cb4ed4ab290526a1f0983535f1e
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2021-02-02 22:13:45 -0500

    adds fetch_question reducer and question store

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,15 @@
 import {
     INCREMENT,
-    DECREMENT
+    DECREMENT,
+    FETCH_QUESTIONS
 } from './actions';
 
 export function counter(state, action) {
     switch(action.type) {
+        case FETCH_QUESTIONS: 
+            return {
+                partnerQuestions: state.partnerQuestions
+            }
         case INCREMENT: 
             return {
                 count: state.count + 1
--- src/store.js
+++ src/store.js
@@ -1,10 +1,45 @@
 import { createStore } from 'redux';
 import { counter } from './reducers';
 
+
+const partnerQuestions = [
+  {
+      questionText: 'you are hangry'
+  },
+  {
+      questionText: 'you are sleepy'
+  },
+  {
+      questionText: 'I said your arms are "weirdly long"'
+  },
+  {
+      questionText: 'I was signing Celine Dion at the top of my lungs during your meeting'
+  },
+  {
+      questionText: 'I ate your leftovers'
+  },
+  {
+      questionText: 'I said the actor in Bridgerton is super hot'
+  },
+  {
+      questionText: 'you are being moody'
+  },
+  {
+      questionText: 'I beat you at ping pong that one time'
+  },
+  {
+      questionText: 'I refuse to do dishes'
+  },
+  {
+      questionText: 'I have been wearing your sweatshirt for the past two weeks'
+  }
+]
+
 //redux 
 
 const defaultState = {
-  count: 0
+  count: 0,
+  partnerQuestions
 };
 
 export const store = createStore(counter, defaultState);
