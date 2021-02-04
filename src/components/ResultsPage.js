ed14935bdea6bca5559e7f6284440902f1ff7ba1
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2021-02-03 22:43:27 -0500

    creates results page and container

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -3,8 +3,7 @@ import React from 'react';
 const ResultsPage = props => (
     <div>
         <h3>
-            You scored 
-            <Output  />.
+            You scored {props.amount}
         </h3>
         <p>
             You are pretty mad.
