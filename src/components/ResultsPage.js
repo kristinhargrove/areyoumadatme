a13a659e2ddc0e99a8ee90997ec1339ae5326665
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2021-02-03 23:08:05 -0500

    adds logic for the quiz to the resultsPage

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
-const ResultsPage = props => (
-    <div>
-        <h3>
-            You scored {props.amount}
-        </h3>
-        <p>
-            You are pretty mad.
-        </p>
-    </div>
-)
+const ResultsPage = props => {
+    let results = '';
+    console.log(props.amount);
+
+    if (props.amount < 5) {
+        results =
+            <div>
+                <p>
+                    A message from your SO: <br />
+                    You are not mad. Go give your SO a hug!
+                </p>
+            </div>
+    } else if (props.amount > 5) {
+        results =
+            <div> 
+                <p>
+                    A message from your SO:<br />
+                    You not really mad, you're probably just a little annoyed. Go buy a latte and calm down.
+                </p>
+            </div>
+    } else {
+        results = 
+            <div>
+                <p>
+                    A message from your SO:<br />
+                    WOW, you are mad. Sorry I made you mad. Do you want to go get margs with me?
+                </p>
+            </div>
+    }
+
+    return (
+        <div className = 'results'>
+            {results}
+        </div>
+    )
+}
 
 export default ResultsPage;
