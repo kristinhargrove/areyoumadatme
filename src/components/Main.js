3d8ded4fe0577aa1797fbdeb4d2aceb4230be76f
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2021-02-04 22:38:16 -0500

    adds Main page styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './Main.css'
-
 import {
     Link
 } from 'react-router-dom';
 
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
 const Main = (props) => (
     <>
-        <body className="main">
-            <h1 className="title">ARE YOU MAD AT ME?</h1>
-            <p>
+    <Container maxWidth="lg">
+        <Typography variant='body1' align="center">
+            <Typography variant='h1'>ARE YOU MAD AT ME?</Typography>
+            <br />
                 "Are you mad at me is a fun and simple assessment to figure out if your SO, friend or family are mad at you. 
                 We are spending A LOT of time with the people we care about, so it makes sense why someone might be mad at you.
                 Simply send them the quiz and you will get the results sent back to you.
-            </p>
-            <p>
+            <br /><br />
                 If they are mad at you, we'll ask them to dinner or to hang out with you, on your behalf. We won't be paying for dinner though, sorry.
-            </p>
-            <Link to='/quiz'><button className="start">Start</button></Link>
-        </body>
+            <br /><br />
+            <Link to='/quiz'><Button variant="contained" size='large'>Start</Button></Link>
+        </Typography>
+    </Container>
     </>
 );
 
