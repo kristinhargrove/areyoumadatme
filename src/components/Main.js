609082083d0236f35a62fa3ef0f7b58f9ebd7442
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2021-02-06 11:13:45 -0500

    fixes more styling on the main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Main.css'
+import './Main.css';
 import {
     Link
 } from 'react-router-dom';
@@ -12,13 +12,21 @@ const Main = (props) => (
     <>
     <Container maxWidth="lg">
         <Typography variant='body1' align="center">
-            <Typography variant='h1'>ARE YOU MAD AT ME?</Typography>
+            <Typography variant='h3'>
+                ARE YOU...
+            </Typography>
             <br />
-                "Are you mad at me is a fun and simple assessment to figure out if your SO, friend or family are mad at you. 
-                We are spending A LOT of time with the people we care about, so it makes sense why someone might be mad at you.
+            <Typography variant='h2'>
+                mad at me?
+            </Typography>
+            <br />
+                Are you mad at me is a fun and simple assessment to figure out if your significant other, friend or family are mad at you. 
+                With COVID, we are spending A LOT of time with the people we care about, so it makes sense why someone might be mad at you.
                 Simply send them the quiz and you will get the results sent back to you.
-            <br /><br />
-                If they are mad at you, we'll ask them to dinner or to hang out with you, on your behalf. We won't be paying for dinner though, sorry.
+                <br /><br />
+                If they are mad at you, we'll ask them to dinner or to hang out with you, on your behalf. 
+                <br />
+                DISCLAIMER: We won't be paying for dinner though, sorry.
             <br /><br />
             <Link to='/quiz'><Button variant="contained" size='large'>Start</Button></Link>
         </Typography>
