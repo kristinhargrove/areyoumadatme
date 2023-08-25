e6a7c13afaeb81b2768afa165e13da206f7617df
Kristin Hargrove <kristin.d.hargrove@gmail.com>
2023-08-25 10:06:37 -0700

    adding photos for quiz and updating header file names to nav file names

---
+++ src/components/Nav.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./Header.css";
+import "../App.scss";
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  Switch,
+  Route,
+  Navigate,
+  Link,
+} from "react-router-dom";
+
+const Nav = () => (
+  <nav>
+    <Link color="inherit" to="/">
+      home
+    </Link>
+    /
+    <Link color="inherit" to="/about">
+      about
+    </Link>
+    /
+    <Link color="textPrimary" to="/quiz">
+      quiz
+    </Link>
+  </nav>
+);
+
+export default Nav;
